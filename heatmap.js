a42df0cb10025def3a96337722e4bb3a294b627f
John Comeau <jc@unternet.net>
2020-05-28 09:14:59 -0700

    working on improvements

--- heatmap.js
+++ heatmap.js
@@ -31,25 +31,11 @@
 */
 
 console.log('heatmap.js loading');
-cjc.rgbcolor = function(string) {
-  return string.replace(/[^\d,]/g, '').split(',');
-}
-cjc.rgbstring = function(array) {
-  return 'rgb(' + array[0] + ',' + array[1] + ',' + array[2] + ')';
-}
-cjc.shuffle = function(array) {
-  array.sort(() => Math.random() - 0.5);
-}
-cjc.mapping = {
-  '46113': {id: '46102', label: 'Oglala Lakota, SD'}
-};
-cjc.debug = function(key) {
-  if (cjc.debugging && (cjc.debugging == true || key == cjc.debugging)) {
-    console.log(Array.from(arguments).slice(1));
-  }
-};
 cjc.selection = null;
 cjc.selected = null;
+cjc.algorithm = null;
+cjc.smoothing = null;
+cjc.scaling = null;
 cjc.header = cjc.CovidConfirmedUsafacts[0];
 cjc.data = cjc.CovidConfirmedUsafacts.slice(1);
 cjc.changed = null;
@@ -66,6 +52,7 @@ cjc.dataOffset = null;
 cjc.state = null;
 cjc.dateIndex = 0;
 cjc.countyIndex = 0;
+cjc.color = null;  // algorithm to use when coloring
 cjc.delay = 1; // ms between iterations
 //developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
 cjc.difference = function(set0, set1) {
@@ -75,9 +62,39 @@ cjc.difference = function(set0, set1) {
   }
   return _difference
 }
+cjc.rgbcolor = function(string) {
+  return string.replace(/[^\d,]/g, '').split(',');
+}
+cjc.rgbstring = function(array) {
+  return 'rgb(' + array[0] + ',' + array[1] + ',' + array[2] + ')';
+}
+cjc.shuffle = function(array) {
+  array.sort(() => Math.random() - 0.5);
+}
+cjc.mapping = {
+  '46113': {id: '46102', label: 'Oglala Lakota, SD'}
+};
+cjc.debug = function(key) {
+  if (cjc.debugging && (cjc.debugging == true || key == cjc.debugging)) {
+    console.log(...Array.from(arguments).slice(1));
+  }
+};
+cjc.sum = function(accumulator, value) {
+  return accumulator + value;
+};
+cjc.min = function(accumulator, value) {
+  return Math.min(accumulator, value);
+};
+cjc.max = function(accumulator, value) {
+  return Math.max(accumulator, value);
+};
 cjc.init = function() {
-  cjc.selection = document.getElementById('datafile');
+  cjc.selection = document.getElementById('data-file');
   cjc.selected = cjc.selection[cjc.selection.selectedIndex].value;
+  cjc.algorithm = document.getElementById('data-algorithm');
+  cjc.color = cjc[cjc.algorithm[cjc.algorithm.selectedIndex].value];
+  cjc.smoothing = document.getElementById('data-smoothing');
+  cjc.scaling = document.getElementById('data-scaling');
   if (cjc.selected == 'CovidTestUsafacts') cjc.delay = 1000;
   cjc.header = cjc[cjc.selected][0];
   cjc.data = cjc[cjc.selected].slice(1)
@@ -94,21 +111,88 @@ cjc.init = function() {
   } else {
     console.log('unexpected header[0]', cjc.header[0]);
   }
+  cjc.smoothData(parseInt(cjc.smoothing[cjc.smoothing.selectedIndex].value));
+  cjc.scaleData(cjc.scaling[cjc.scaling.selectedIndex].value == "true");
+  cjc.color(null, true);  // run initialization for color algorithm
   cjc.dateIndex = cjc.dataOffset;
   cjc.countyIndex = 0;
   cjc.shuffle(cjc.data);
   cjc.changed = cjc.data.filter(row => row[cjc.dateIndex] > 0);
   if (cjc.changed.length == 0) cjc.changed = cjc.data.slice(0, 1);
 };
+cjc.smoothData = function(days) {
+  if (days == 0) return;
+  for (let i = 0; i < cjc.data.length; i++) {
+    cjc.movingAverage(cjc.data[i], days);
+  }
+};
+cjc.scaleData = function(choice) {
+  if (choice) {
+    for (let i = 0; i < cjc.data.length; i++) {
+      cjc.byPopulation(cjc.data[i]);
+    }
+  }
+};
 cjc.debugNext = function() {
+  // call this from console
   cjc.init();
   savedDebugging = cjc.debugging;
   cjc.debugging = true;
   cjc.next();
   cjc.debugging = savedDebugging;
 };
-cjc.next = function() {
+cjc.naive = function(path, init) {
+  // this was my first, unscientific, stab at a "heat" plot
+  if (init) return;  // this does not use an initialization routine
+  cjc.area.value = path.getAttribute('title');
   let red = 0, green = 0, blue = 0;
+  let color = path.style.fill;
+  let lastCount = parseFloat(path.getAttribute('data-count')) || 0;
+  let weight = parseFloat(path.getAttribute('data-weight')) || -1;
+  let count = cjc.changed[cjc.countyIndex][cjc.dateIndex];
+  cjc.debug('color', 'color', color, 'weight', weight, 'count', count,
+            'lastCount', lastCount);
+  if (count > lastCount) {
+    cjc.debug(true, path.getAttribute('title') + ' count ' + count +
+                ' > ' + lastCount);
+    // cap the weight such that hotspots that are cooling down will show that
+    if (weight > -1) weight = Math.min(weight + 3, 30);
+    else weight = 3;
+  } else if (weight > 0) {
+    weight -= .3;  // over 10 days we lose the last gain
+    if (weight < 0.1) weight = 0;
+  }
+  // varies shade from green to brown to red and vice versa on cooling
+  red = Math.round(weight) > -1 ? Math.round(Math.min(10 * weight, 255)) : 0;
+  green = Math.round(255 - red);
+  blue = 0;
+  cjc.debug('color', 'weight ' + weight + ', rgb: ' +
+            red + ', ' + green + ', ' + blue);
+  path.setAttribute('data-weight', weight);
+  path.setAttribute('data-count', count);
+  if (weight > -1) {  // leave -1 at default color
+    color = cjc.rgbstring([red, green, blue]);
+  }
+  path.style.fill = color;
+};
+cjc.movingAverage = function(row, days) {
+  for (let i = row.length - 1; i > cjc.dataOffset; i--) {
+    sample = row.slice(Math.max(cjc.dataOffset, i - days), i);
+    cjc.debug('average', 'sample', sample);
+    row[i] = sample.reduce(cjc.sum) / sample.length;
+  }
+};
+cjc.byPopulation = function(row) {
+  const key = row[0];
+  cjc.debug('population', row, key);
+  const population = cjc.CovidCountyPopulationUsafacts
+    .filter(function(row) {return row[0] == key})[0][3];
+  cjc.debug('population', 'population: ' + population);
+  for (let i = row.length - 1; i >= cjc.dataOffset; i--) {
+    row[i] = row[i] / population;
+  }
+};
+cjc.next = function() {
   let path = null;
   let countyFips = cjc.changed[cjc.countyIndex][0];
   let date = cjc.header[cjc.dateIndex];
@@ -117,40 +201,12 @@ cjc.next = function() {
   try {
     path = cjc.paths[countyFips];
   } catch (error) {
-    console.log('null path for id ' + countyFips, error);
+    cjc.debug(true, 'null path for id ' + countyFips, error);
   }
   if (path != null) {
-    cjc.area.value = path.getAttribute('title');
-    let color = path.style.fill;
-    let lastCount = parseFloat(path.getAttribute('data-count')) || 0;
-    let weight = parseFloat(path.getAttribute('data-weight')) || -1;
-    let count = cjc.changed[cjc.countyIndex][cjc.dateIndex];
-    cjc.debug('color', 'color', color, 'weight', weight, 'count', count,
-              'lastCount', lastCount);
-    if (count > lastCount) {
-      console.log(path.getAttribute('title') + ' count ' + count +
-                  ' > ' + lastCount);
-      // cap the weight such that hotspots that are cooling down will show that
-      if (weight > -1) weight = Math.min(weight + 3, 30);
-      else weight = 3;
-    } else if (weight > 0) {
-      weight -= .3;  // over 10 days we lose the last gain
-      if (weight < 0.1) weight = 0;
-    }
-    // varies shade from green to brown to red and vice versa on cooling
-    red = Math.round(weight) > -1 ? Math.round(Math.min(10 * weight, 255)) : 0;
-    green = Math.round(255 - red);
-    blue = 0;
-    cjc.debug('color', 'weight ' + weight + ', rgb: ' +
-              red + ', ' + green + ', ' + blue);
-    path.setAttribute('data-weight', weight);
-    path.setAttribute('data-count', count);
-    if (weight > -1) {  // leave -1 at default color
-      color = cjc.rgbstring([red, green, blue]);
-    }
-    path.style.fill = color;
+    cjc.color(path);
   } else {
-    console.log('key ' + countyFips + ' has no matching <path>');
+    cjc.debug(true, 'key ' + countyFips + ' has no matching <path>');
   }
   cjc.countyIndex++;
   if (cjc.countyIndex == cjc.changed.length) {
@@ -160,7 +216,7 @@ cjc.next = function() {
       cjc.dateIndex = cjc.dataOffset;
       cjc.state = 'finished';
     } else {
-      console.log('starting ' + cjc.header[cjc.dateIndex]);
+      cjc.debug(true, 'starting ' + cjc.header[cjc.dateIndex]);
       cjc.shuffle(cjc.data);
       cjc.changed = cjc.data.filter(row => row[cjc.dateIndex] > 0);
       if (cjc.changed.length == 0) cjc.changed = cjc.data.slice(0, 1);
