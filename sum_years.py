7094dfd2a074d130b2d7e7d8cc1366667b67677b
John Comeau <jc@unternet.net>
2021-10-28 19:14:05 -0700

    backwards compatible with older Python versions

--- sum_years.py
+++ sum_years.py
@@ -2,7 +2,9 @@
 '''
 Sum years to date from Javascript files
 '''
-import sys, os, re
+import sys, os, re, logging
+
+logging.basicConfig(level=logging.DEBUG if __debug__ else logging.INFO)
 
 def sum_years(jsfile, year0, year1, cutoff=None):
     '''
@@ -23,12 +25,13 @@ def sum_years(jsfile, year0, year1, cutoff=None):
         year1 = year1[:year1.index(None)]
         cutoff = data[len(year1) - 1][0]
     else:
-        cutoff = f' {cutoff}'
+        cutoff = ' {}'.format(cutoff)
         year1 = year1[:cutoffs.index(cutoff) + 1]
     year0 = [data[n][index0 + 1] for n in range(len(year1))]
+    logging.debug('year0: %s, year1: %s', year0, year1)
     sums = sum(year0), sum(year1)
     ratio = sums[1] / sums[0]
-    return sums +  (ratio, f'through week {cutoff.strip()}')
+    return sums +  (ratio, 'through week {}'.format(cutoff.strip()))
 
 if __name__ == '__main__':
     print(sum_years(*sys.argv[1:]))
